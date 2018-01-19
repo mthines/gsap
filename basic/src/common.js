d2533d8a24495743c7c561deec9b1ab53e179c98
mads-thines <mads.thines@adapt.dk>
2018-01-19 14:46:11 +0100

    Initial GSAP Test Commit

---
+++ basic/src/common.js
@@ -0,0 +1,4 @@
+export const isTouchDevice = () => {
+  return 'ontouchstart' in window        // works on most browsers
+    || navigator.maxTouchPoints;         // works on IE10/11 and Surface
+};
